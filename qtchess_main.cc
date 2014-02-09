c02ac45d1f65b458dc26c395e07399c1a965ac11
textfield <textfield@188c89f1-0f9f-4e6d-a634-c9499bad2055>
2014-02-09 15:54:18 +0000

    Dates.

--- qtchess_main.cc
+++ qtchess_main.cc
@@ -1,5 +1,5 @@
 /*
-** Copyright (c) 2003 - 2014 Alexis Megas
+** Copyright (c) 2003 - Eternity Alexis Megas
 ** All rights reserved.
 **
 ** Redistribution and use in source and binary forms, with or without
