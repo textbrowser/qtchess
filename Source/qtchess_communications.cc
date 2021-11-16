7adc5be54188acdadaefa453747f3f1542152ce8
textbrowser@gmail.com <textbrowser@gmail.com>
2021-11-15 20:50:46 -0500

    For Mac.

--- Source/qtchess_communications.cc
+++ Source/qtchess_communications.cc
@@ -114,11 +114,15 @@ QHostAddress qtchess_communications::preferred_host_address
 (const QAbstractSocket::NetworkLayerProtocol protocol)
 {
   foreach(const auto &interface, QNetworkInterface::allInterfaces())
-    if(!(interface.flags() & QNetworkInterface::IsLoopBack))
-      if(interface.flags() & QNetworkInterface::IsUp)
-	foreach(const auto &address, interface.addressEntries())
-	  if(address.ip().protocol() == protocol)
-	    return address.ip();
+    {
+      if(!(interface.flags() & QNetworkInterface::IsLoopBack))
+	if(interface.flags() & QNetworkInterface::IsUp)
+	  foreach(const auto &address, interface.addressEntries())
+	    {
+	      if(address.ip().protocol() == protocol)
+		return address.ip();
+	    }
+    }
 
   if(protocol == QAbstractSocket::IPv4Protocol)
     return QHostAddress(QHostAddress::LocalHost);
