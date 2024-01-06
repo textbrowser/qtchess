6c315fcf4794aafa3e8f2145a6c0c1d6016eddf3
textbrowser <textbrowser@gmail.com>
2024-01-06 17:51:12 -0500

    New files.

--- Distributions/build_debian_arm32.sh
+++ Distributions/build_debian_arm32.sh
@@ -1,4 +1,22 @@
-Preparing /usr/local/qtchess:
+#!/usr/bin/env sh
+# Alexis Megas.
+
+if [ ! -x /usr/bin/dpkg-deb ]; then
+    echo "Please install dpkg-deb."
+    exit
+fi
+
+if [ ! -x /usr/bin/fakeroot ]; then
+    echo "Please install fakeroot."
+    exit 1
+fi
+
+if [ ! -r qtchess.pro ]; then
+    echo "Please execute $0 from the primary directory."
+    exit 1
+fi
+
+# Prepare local files.
 
 mkdir -p ./usr/local/qtchess
 qmake -o Makefile qtchess.pro && make -j $(nproc)
@@ -6,7 +24,7 @@ cp ./QtChess ./usr/local/qtchess/.
 cp ./qtchess.sh ./usr/local/qtchess/.
 find ./usr/local/qtchess -type f -exec chmod g+w {} \;
 
-Preparing QtChess-2024.01.10_armhf.deb:
+# Prepare the Debian file.
 
 mkdir -p qtchess-debian/usr/local
 cp -pr ./RASPBIAN qtchess-debian/DEBIAN
