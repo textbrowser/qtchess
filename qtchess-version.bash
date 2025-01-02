e0a1a5f099d443523ceee6de1491c2c459bb4d72
textbrowser <textbrowser@gmail.com>
2025-01-01 19:46:20 -0500

    Nicer format.

--- qtchess-version.bash
+++ qtchess-version.bash
@@ -4,16 +4,19 @@ echo "The command sed may fail on MacOS."
 
 VERSION=$1
 
-if [ -z "$VERSION" ]; then
+if [ -z "$VERSION" ]
+then
     echo "Please specify the version: $0 <VERSION>."
     exit 1
 fi
 
-for file in */control; do
+for file in */control
+do
     sed -i "s/Version: .*/Version: $VERSION/" $file
 done
 
-for file in Distributions/build*; do
+for file in Distributions/build*
+do
     sed -i \
     "s/QtChess-[[:digit:]]\+\.[[:digit:]]\+\.[[:digit:]]\+/QtChess-$VERSION/" \
     $file
