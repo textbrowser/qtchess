836d1029e0597aae7856e30a569d09a62d1c57e7
textbrowser <textbrowser@gmail.com>
2025-01-14 19:38:02 -0500

    New file.

---
+++ Distributions/prepare_apk_name.bash
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+VERSION=$1
+
+if [ -z "$VERSION" ]
+then
+    echo "Please specify the version: $0 <VERSION>."
+    exit 1
+fi
+
+for i in $(find . -iname '*.apk')
+do
+    if [[ "$i" == *"arm64"* ]]
+    then
+	mv $i ~/QtChess-"$VERSION"_arm64.apk
+    fi
+
+    if [[ "$i" == *"armeabi"* ]]
+    then
+	mv $i ~/QtChess-"$VERSION"_arm32.apk
+    fi
+done
