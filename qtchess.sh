13096c42b944df603430dc5754e8c77557ca26f9
textbrowser <textbrowser@gmail.com>
2026-01-06 08:57:52 -0500

    Qt 6.

--- qtchess.sh
+++ qtchess.sh
@@ -2,6 +2,21 @@
 
 # Alexis Megas.
 
+set_qt_qpa_platformtheme()
+{
+    qt6=$(ldd "$1" | grep Qt6 2>/dev/null)
+
+    if [ ! -z "$qt6" ]
+    then
+	if [ ! -z "$(which qt6ct)" ]
+	then
+	    echo "Exporting QT_QPA_PLATFORMTHEME as qt6ct."
+
+	    export QT_QPA_PLATFORMTHEME=qt6ct
+	fi
+    fi
+}
+
 export QT_AUTO_SCREEN_SCALE_FACTOR=1
 
 # Disable https://en.wikipedia.org/wiki/MIT-SHM.
@@ -11,12 +26,14 @@ export QT_X11_NO_MITSHM=1
 if [ -r ./QtChess ] && [ -x ./QtChess ]
 then
     echo "Launching a local QtChess."
+    set_qt_qpa_platformtheme "./QtChess"
     ./QtChess "$@"
     exit $?
 elif [ -r /opt/qtchess/QtChess ] && [ -x /opt/qtchess/QtChess ]
 then
     echo "Launching an official QtChess."
-    cd /opt/qtchess && ./QtChess "$@"
+    set_qt_qpa_platformtheme "/opt/qtchess/QtChess"
+    /opt/qtchess/QtChess "$@"
     exit $?
 else
     echo "Cannot locate QtChess."
